115f4ec3663da07f8a73ac66b551eb6ebd8da66e
nihar patel <niharpatel2545@gmail.com>
2025-07-01 18:41:01 +0530

    added crossenv to replace dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
+import dotenv from 'dotenv';
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
@@ -18,7 +19,7 @@ const __dirname = path.resolve();
 
 app.use(express.json()); // will allow us to parse req.body
 app.use(cookieParser());
-
+dotenv.config();
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
