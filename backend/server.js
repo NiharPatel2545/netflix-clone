dd709c8822614f4d998718ee72ab4640be49b65c
nihar patel <niharpatel2545@gmail.com>
2025-07-17 18:41:24 +0530

    changes to server js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
-import dotenv from 'dotenv';
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
@@ -19,7 +18,7 @@ const __dirname = path.resolve();
 
 app.use(express.json()); // will allow us to parse req.body
 app.use(cookieParser());
-dotenv.config();
+
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
